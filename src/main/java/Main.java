012265a4564698340c0b41dcec60e5c7fa7487b1
Nitesh <kaindalnitesh909@gmail.com>
2025-07-01 00:28:19 +0530

    added no command found

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
 
-        while(true){
-            String input = scanner.nextLine();
-            System.out.println("$"+input+": command not found");
-        }
+       System.out.print("$ ");
+       Scanner scanner = new Scanner(System.in);
+       String input = scanner.nextLine();
+       System.out.println(input + ": command not found");
+
+        
     }
 }
