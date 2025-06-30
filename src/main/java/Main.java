0a446be60e0223e58dc13bdd80eccb3540b89be4
Nitesh <kaindalnitesh909@gmail.com>
2025-07-01 00:18:30 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
         while(true){
+            String input = scanner.nextLine();
             System.out.println(input+": command not found");
         }
     }
